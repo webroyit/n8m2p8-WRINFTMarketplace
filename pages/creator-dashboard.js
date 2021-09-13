e14a4ccc805e9d75b53cf4940be5e9f73fdbda98
royweb <xytank@gmail.com>
2021-09-13 14:10:31 -0400

    Add function to fetch NFTs created by user

--- pages/creator-dashboard.js
+++ pages/creator-dashboard.js
@@ -1,34 +1,57 @@
 // pages/creator-dashboard.js
+import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from "web3modal"
+
+import {
+  nftmarketaddress, nftaddress
+} from '../config'
+
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
 
 export default function CreatorDashboard() {
   const [nfts, setNfts] = useState([])
   const [sold, setSold] = useState([])
+  const [loadingState, setLoadingState] = useState('not-loaded')
 
   useEffect(() => {
-    setNfts([
-      {
-        price: '1',
-        tokenId: 1,
-        seller: "0x0wef2131",
-        owner: "0x0123123123",
-        image: "https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1366&q=80",
-        name: "Flower",
-        description: "It is a flower",
-      }
-    ])
-    setSold([
-      {
-        price: '1',
-        tokenId: 1,
-        seller: "0x0wef2131",
-        owner: "0x0123123123",
-        image: "https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1366&q=80",
-        name: "Flower",
-        description: "It is a flower",
-      }
-    ])
+    loadNFTs()
   }, [])
+
+  async function loadNFTs() {
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
+    const data = await marketContract.fetchItemsCreated()
+
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await tokenContract.tokenURI(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        sold: i.sold,
+        image: meta.data.image,
+      }
+      return item
+    }))
+    // Create a filtered array of items that have been sold
+    const soldItems = items.filter(i => i.sold)
+    setSold(soldItems)
+    setNfts(items)
+    setLoadingState('loaded') 
+  }
+
+  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No assets created</h1>)
 
   return (
     <div>
@@ -47,22 +70,26 @@ export default function CreatorDashboard() {
           }
         </div>
       </div>
-        <div className="px-4">
-          <div>
-            <h2 className="text-2xl py-2">Items sold</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-              {
-                sold.map((nft, i) => (
-                  <div key={i} className="border shadow rounded-xl overflow-hidden">
-                    <img src={nft.image} className="rounded" />
-                    <div className="p-4 bg-black">
-                      <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+      <div className="px-4">
+        {
+          Boolean(sold.length) && (
+            <div>
+              <h2 className="text-2xl py-2">Items sold</h2>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+                {
+                  sold.map((nft, i) => (
+                    <div key={i} className="border shadow rounded-xl overflow-hidden">
+                      <img src={nft.image} className="rounded" />
+                      <div className="p-4 bg-black">
+                        <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+                      </div>
                     </div>
-                  </div>
-                ))
-              }
+                  ))
+                }
+              </div>
             </div>
-          </div>
+          )
+        }
       </div>
     </div>
   )
